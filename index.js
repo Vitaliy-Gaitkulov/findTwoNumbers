2e1fb87db089a005084f23ac233f78554735a3f0
Vitaliy-Gaitkulov <iri.vert33@gmail.com>
2024-05-16 00:09:42 +0300

    Refactoring.

--- index.js
+++ index.js
@@ -6,25 +6,16 @@ const ARRAY_MAX_SIZE = 100000000;
   process.stdin.on("data", (data) => {
     const size = Math.round(Number(data));
 
-    if (isNaN(size)) {
-      console.log("Вы ввели не число!");
-      return;
-    }
+    if (isNaN(size)) return console.log("Вы ввели не число!");
 
-    if (size < 2) {
-      console.log("Введите больше 1!");
-      return;
-    }
-    if (size > ARRAY_MAX_SIZE) {
-      console.log("Массив слишком большой! Максимальный размер массива: " + ARRAY_MAX_SIZE);
-      return; 
-    }
-    
+    if (size < 2) return console.log("Введите больше 1!");
+
+    if (size > ARRAY_MAX_SIZE) return console.log("Массив слишком большой! Максимальный размер массива: " + ARRAY_MAX_SIZE);
+
     const TestArray = numbersGenerator(size);
     console.log("исходный массив:\n", TestArray);
     const result = findTwoNumbers(TestArray, size);
     console.log("Hайденные числа:", result);
-    
   });
 })();
 
@@ -32,10 +23,10 @@ const numbersGenerator = (size) => {
   const array = Array.from(Array(size), (_, i) => i + 1);
   const rands = getRandomIndex(size);
   array.splice(rands[0], 1);
-  array.splice(rands[1]-1, 1);
+  array.splice(rands[1] - 1, 1);
 
   return array;
-}
+};
 
 const getRandomIndex = (size) => {
   const numbers = [];
@@ -47,12 +38,12 @@ const getRandomIndex = (size) => {
   }
 
   return numbers;
-}
+};
 
 const findTwoNumbers = (array, arrayLength) => {
-    const result = [];
-  
-    if (array[0] !== 1) {
+  const result = [];
+
+  if (array[0] !== 1) {
       result.push(1);                        // в случае если первого елемента нет
       array.unshift(1);                      // в случае если нет первого и второго елемента
     }
@@ -65,16 +56,15 @@ const findTwoNumbers = (array, arrayLength) => {
         array.push(array[i] + 1);
       } else if (array[i] !== checkNumber) { // стандартное условие
         if (array[i] !== checkNumber - 1) {  // в случае если отсутсвующие числа идут подряд
-          result.push(array[i] + 1, array[i] + 2);
-  
-          return result;
-        }
-        result.push(array[i] + 1);
-      }
-      if (result.length == 2) {
+        result.push(array[i] + 1, array[i] + 2);
+
         return result;
       }
+      result.push(array[i] + 1);
     }
-  };
+    
+    if (result.length == 2) return result;
+  }
+};
 
-  // Вычислительная мощность решения = O(n) n = длинна массива
+// Вычислительная мощность решения = O(n) n = длинна массива
